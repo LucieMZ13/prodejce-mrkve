7fc0b9ac8519d6d6148d1ebb42237ae5b4dd235f
LucieMZ13 <llmrazkova@gmail.com>
2024-10-29 22:45:51 +0100

    hotovo

--- src/CarrotSeller.java
+++ src/CarrotSeller.java
@@ -1,4 +1,67 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class CarrotSeller {
+    private String name;
+    private LocalDate dateOfBirth;
+    private int numberOfContracts;
+    private int carrotsSold;
+    private String cityOfResidence;
+    private String carRegistrationNumber;
+    private double fuelConsumption;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getNumberOfContracts() {
+        return numberOfContracts;
+    }
+
+    public void setNumberOfContracts(int numberOfContracts) {
+        this.numberOfContracts = numberOfContracts;
+    }
+
+    public int getCarrotsSold() {
+        return carrotsSold;
+    }
+
+    public void setCarrotsSold(int carrotsSold) {
+        this.carrotsSold = carrotsSold;
+    }
+
+    public String getCityOfResidence() {
+        return cityOfResidence;
+    }
+
+    public void setCityOfResidence(String cityOfResidence) {
+        this.cityOfResidence = cityOfResidence;
+    }
+
+    public String getCarRegistrationNumber() {
+        return carRegistrationNumber;
+    }
+
+    public void setCarRegistrationNumber(String carRegistrationNumber) {
+        this.carRegistrationNumber = carRegistrationNumber;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CarrotSeller firstCarrotSeller = new CarrotSeller();
+        firstCarrotSeller.setName("Pavel Novák");
+        System.out.println("Jméno prodejce: "+firstCarrotSeller.getName());
+        firstCarrotSeller.setDateOfBirth(LocalDate.of(1989,2,15));
+        System.out.println("Prodejce narozen: "+firstCarrotSeller.getDateOfBirth());
+        firstCarrotSeller.setNumberOfContracts(58);
+        System.out.println("Počet uzavřených smluv: "+firstCarrotSeller.getNumberOfContracts());
+        firstCarrotSeller.setCarrotsSold(13);
+        System.out.println("Prodané mrkve: " +firstCarrotSeller.getCarrotsSold()+" t");
+        firstCarrotSeller.setCityOfResidence("Olomouc");
+        System.out.println("Město: "+firstCarrotSeller.getCityOfResidence());
+        firstCarrotSeller.setCarRegistrationNumber("3MO 8118");
+        System.out.println("Registrační značka vozidla: "+firstCarrotSeller.getCarRegistrationNumber());
+        firstCarrotSeller.setFuelConsumption(6.2);
+        System.out.println("Spotřeba vozidla: "+firstCarrotSeller.getFuelConsumption()+" l/100 km");
+        CarrotSeller secondCarrotSeller = new CarrotSeller();
+        secondCarrotSeller.setName("Jan Pištík");
+        System.out.println("Jméno prodejce: "+secondCarrotSeller.getName());
+        secondCarrotSeller.setDateOfBirth(LocalDate.of(1992,8,5));
+        System.out.println("Prodejce narozen: "+secondCarrotSeller.getDateOfBirth());
+        secondCarrotSeller.setNumberOfContracts(29);
+        System.out.println("Počet uzavřených smluv: "+secondCarrotSeller.getNumberOfContracts());
+        secondCarrotSeller.setCarrotsSold(8);
+        System.out.println("Prodané mrkve: "+secondCarrotSeller.getCarrotsSold()+ " t");
+        secondCarrotSeller.setCityOfResidence("Brno");
+        System.out.println("Město: "+secondCarrotSeller.getCityOfResidence());
+        secondCarrotSeller.setCarRegistrationNumber("8A2 1111");
+        System.out.println("Registrační značka vozidla: "+secondCarrotSeller.getCarRegistrationNumber());
+        secondCarrotSeller.setFuelConsumption(4.1);
+        System.out.println("Spotřeba vozidla: "+secondCarrotSeller.getFuelConsumption()+" l/100 km");
     }
 }
